455aa253807da91b2ceaf46a379aacd627b2a61a
Nicolas Lohya <nicolas.lohya.pro@gmail.com>
2023-12-15 13:17:25 +0000

    php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+try {
+    $conn = new PDO("mysql:host=terraform-20231215100811342300000002.cjd61w5im1sh.eu-north-1.rds.amazonaws.com;dbname=terrdb", "db_aws_user", "4lNVZCWo7fD8RKq8ITognZBFSqaxNU4DdytlYP7W");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+if (isset($_GET["name"])) {
+    $sql = "INSERT INTO utilisateurs (nom) VALUES (?)";
+    $conn->prepare($sql)->execute([$_GET["name"]]);
+}
+
+$conn->query("CREATE TABLE utilisateurs (nom VARCHAR(50))")->execute();
+
+$stmt = $conn->query("SELECT (nom) FROM utilisateurs");
+while ($row = $stmt->fetch()) {
+    echo $row['nom']."<br />\n";
+}
+?>
+<form method="GET">
+    <input type="text" placeholder="Nom" name="name" />
+    <input type="submit" value="ajouter le monsieur" />
+</form>

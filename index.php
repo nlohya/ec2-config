7453ff8024e48a997563c9c47d14f1a4ea1026af
Nicolas Lohya <nicolas.lohya.pro@gmail.com>
2023-12-15 13:36:40 +0000

    azaz

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <h1>Bonjour !</p>
+
 <?php
 
 try {
     $conn = new PDO("mysql:host=terraform-20231215100811342300000002.cjd61w5im1sh.eu-north-1.rds.amazonaws.com;dbname=terrdb", "db_aws_user", "4lNVZCWo7fD8RKq8ITognZBFSqaxNU4DdytlYP7W");
+    var_dump($conn);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
+    var_dump("coucou");
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
@@ -25,7 +28,9 @@ $stmt = $conn->query("SELECT (nom) FROM utilisateurs");
 while ($row = $stmt->fetch()) {
     echo $row['nom']."<br />\n";
 }
+
 ?>
+
 <form method="GET">
     <input type="text" placeholder="Nom" name="name" />
     <input type="submit" value="ajouter le monsieur" />
